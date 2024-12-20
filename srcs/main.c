986b1998ec4e496438776792b20a039e936272d4
Wojtek Kornatowski <wojkor338@gmail.com>
2024-12-20 10:59:17 +0100

    updated main.c

--- srcs/main.c
+++ srcs/main.c
@@ -3,18 +3,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+short	**create_array(unsigned int rows, unsigned int cols)
+{
+	short	**result;
+
+	result = (short **)calloc(sizeof(short *), cols);
+	if (!result)
+		exit(EXIT_FAILURE);
+	for (int i = 0; i < cols; i++)
+	{
+		result[i] = (short *)calloc(sizeof(short), rows);
+		if (!result[i])
+		{
+			for (int j = 0; j < i; j++)
+				free(result[i]);
+			free(result);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (result);
+}
+
 int main(void)
 {
 	short	**grid;
+	int		cols;
+	int		rows;
 
-	grid = (short **)calloc(sizeof(short *), cols);
-	if (!grid)
-		//handle memory failure
-	for (int i = 0; i < rows; i++)
-		grid[i] = (short *)calloc(sizeof(short), rows);
-		if (!grid[i])
-			//handle memory failure
-	//create array
+	grid = create_array(rows, cols);
 	//generate tiles
 	//start game
 }
